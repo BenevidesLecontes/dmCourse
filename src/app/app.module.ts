c339924ef1c8688069e60a3d5f940dbdacc12fea
Benevides Lecontes <beny1000@hotmail.com>
2018-02-24 10:23:10 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MaterialModule} from './material/material.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {Route, RouterModule} from '@angular/router';
+import {TransacoesComponent} from './transacoes/transacoes.component';
+import {BeneficiosComponent} from './beneficios/beneficios.component';
+import {HistoricoComponent} from './historico/historico.component';
 
+const routes: Route[] = [
+  {path: 'transacoes', component: TransacoesComponent},
+  {path: 'beneficios', component: BeneficiosComponent},
+  {
+    path: 'historico',
+    component: HistoricoComponent
+  },
+  {
+    path: '',
+    redirectTo: '/transacoes',
+    pathMatch: 'full'
+  },
+  {path: '**', component: TransacoesComponent}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransacoesComponent,
+    BeneficiosComponent,
+    HistoricoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
